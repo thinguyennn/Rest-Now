7e6fa1ec1244ecc0f97156618465f6ee62ed9826
Kausthub Jadhav <j@dhav.me>
2026-02-07 11:55:41 +0530

    Update cask SHA256 for notarized v1.0.1 DMG

--- restnow.rb
+++ restnow.rb
@@ -1,6 +1,6 @@
 cask "restnow" do
   version "1.0.1"
-  sha256 "24d2a17e1ebcaab4ec7e88c51f2f231157a08e43174b201f263dbd0582bad3ab"
+  sha256 "dc1cf33f2532f921d0e91e7880c17f19c4f19ef6ac1db162bd82ba53e3354aae"
 
   url "https://github.com/krjadhav/Rest-Now/releases/download/v#{version}/RestNow.dmg"
   name "RestNow"
